b73d966d7c92c68aa9f9e54dfa99f6df9a4b402f
Michal <kowalski4306@gmail.com>
2020-05-02 13:22:43 +0200

    [fixture] multiplication part done

--- calcTests.cpp
+++ calcTests.cpp
@@ -7,31 +7,37 @@ public:
 	Calculator calc;
 };
 
-TEST_F(calcFixture, additionMethodTest)
+TEST_F(calcFixture, additionMethodValueTest0)
+{
+	double a = 1.1, b = 0;
+	EXPECT_DOUBLE_EQ(calc.addition(a,b), 1.1);
+}
+
+TEST_F(calcFixture, additionMethodValueTest1)
 {
 	double a = 1.1, b = 2.2;
 	EXPECT_DOUBLE_EQ(calc.addition(a,b), 3.3);
 }
 
-TEST_F(calcFixture, subtractionMethod0ValueTest)
+TEST_F(calcFixture, subtractionMethodValueTest0)
 {
 	double a = 3.3, b = 0;
 	EXPECT_DOUBLE_EQ(calc.subtraction(a,b), 3.3);
 }
 
-TEST_F(calcFixture, subtractionMethodTest)
+TEST_F(calcFixture, subtractionMethodValueTest1)
 {
 	double a = 1.1, b = 2.2;
 	EXPECT_DOUBLE_EQ(calc.subtraction(a,b), -1.1);
 }
 
-TEST_F(calcFixture, multiplicationMethod0ValueTest)
+TEST_F(calcFixture, multiplicationMethodValueTest0)
 {
 	double a = 1.1, b = 0;
 	EXPECT_DOUBLE_EQ(calc.multiplication(a,b), 0);
 }
 
-TEST_F(calcFixture, multiplicationMethod1ValueTest)
+TEST_F(calcFixture, multiplicationMethodValueTest1)
 {
 	double a = 3.3, b = 5;
 	EXPECT_DOUBLE_EQ(calc.multiplication(a,b), 16.5);
--- mainFunction.cpp
+++ mainFunction.cpp
@@ -6,11 +6,15 @@ void mainFunction()
 {
 	//1. Calculator's Class Test
 	Calculator calc;
+	std::cout << "10.1 + 0 = " << calc.addition(10.1, 0) << std::endl;
 	std::cout << "10.1 + 5.5 = " << calc.addition(10.1, 5.5) << std::endl;
 
-	std::cout << "5.5 + 2.2 = " << calc.subtraction(5.5, 2.2) << std::endl;
+	std::cout << "5.5 - 0 = " << calc.subtraction(5.5, 0) << std::endl; 
+	std::cout << "5.5 - 2.2 = " << calc.subtraction(5.5, 2.2) << std::endl;
 
-	//..dopisz odejmowanie, mnozenie, dzielenie
+	std::cout << "3.3 * 0 = " << calc.multiplication(3.3, 0) << std::endl;
+	std::cout << "3.3 * 1.1 = " << calc.multiplication(3.3, 1.1) << std::endl;
+	//..dzielenie
 	//
 	//na koniec zrob mocka
 }
